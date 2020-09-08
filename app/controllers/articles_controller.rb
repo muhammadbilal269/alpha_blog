cac9b362fbdb5f732c011f1a3c5d5794c95f7212
rails course <bilal.khan535765@gmail.com>
2020-09-08 16:56:15 +0500

    add edit, uptdate , show and listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,22 @@ class ArticlesController <ApplicationController
 def new
 @article = Article.new
 end
+def edit
+@article = Article.find(params[:id])
+end
+def update
+@article = Article.find(params[:id])
+if @article.update(article_params)
+	flash[:notice] = "Article is successfully updated"
+	redirect_to article_path(@article)
+else
+	render 'edit'
+end
+end
 
-
-
+def index
+@articles = Article.all
+end
 
 def create
 	@article = Article.new(article_params)
