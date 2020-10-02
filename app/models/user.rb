b21713546554cadbb1cb624653a5276137b946f2
rails course <bilal.khan535765@gmail.com>
2020-10-02 13:38:03 +0500

    add validations for email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User <ActiveRecord::Base
-
+validates :username, presence: true,
+ uniqueness:{case_sensitive:false } ,
+  length: {minimum: 3 , maximum: 25}
+  VALID_EMAIL_REJEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, 
+  length: {maximum:100},
+  uniqueness: {case_sensitive: false},
+  format: {with: VALID_EMAIL_REJEX}
 end
